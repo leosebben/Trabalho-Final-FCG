fc4bc83a0a197572a5177b6e8696de68200784b7
leosebben <68429302+leosebben@users.noreply.github.com>
2022-09-21 08:36:04 -0300

    feat(src/main): add base headers

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// Headers originais de C
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+
+// Headers abaixo são específicos de C++
+#include <map>
+#include <string>
+#include <limits>
+#include <fstream>
+#include <sstream>
+
+// Headers das bibliotecas OpenGL
+#include <glad/glad.h>   // Criação de contexto OpenGL 3.3
+#include <GLFW/glfw3.h>  // Criação de janelas do sistema operacional
+
+// Headers da biblioteca GLM: criação de matrizes e vetores.
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Headers locais, definidos na pasta "include/"
+#include "utils.h"
+#include "matrices.h"

d6941601222b11ef0cd70ee9b60f36589b6a43cc
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2022-10-02 01:53:01 -0300

    feat(obj): new objects

--- src/shader_fragment.glsl
+++ src/shader_fragment.glsl
@@ -14,9 +14,11 @@ uniform mat4 view;
 uniform mat4 projection;
 
 // Identificador que define qual objeto está sendo desenhado no momento
-#define SPHERE 0
-#define BUNNY  1
-#define PLANE  2
+#define SPHERE  0
+#define BUNNY   1
+#define PLANE   2
+#define BALOON  3
+#define MFOUR  4
 uniform int object_id;
 
 // O valor de saída ("out") de um Fragment Shader é a cor final do fragmento.
@@ -81,6 +83,24 @@ void main()
         Ks = vec3(0.3, 0.3, 0.3);
         Ka = vec3(0.0, 0.0, 0.0);
         q = 20.0;
+    }
+    else if ( object_id == BALOON )
+    {
+        // PREENCHA AQUI
+        // Propriedades espectrais do coelho
+        Kd = vec3(0.08, 0.4, 0.8);
+        Ks = vec3(0.8,  0.8, 0.8);
+        Ka = vec3(0.04, 0.2, 0.4);
+        q = 32.0;
+    }
+    else if ( object_id == MFOUR )
+    {
+        // PREENCHA AQUI
+        // Propriedades espectrais do coelho
+        Kd = vec3(0.08, 0.4, 0.8);
+        Ks = vec3(0.8,  0.8, 0.8);
+        Ka = vec3(0.04, 0.2, 0.4);
+        q = 32.0;
     }
     else // Objeto desconhecido = preto
     {
